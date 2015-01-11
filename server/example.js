dcc6f6b1f2199a09669324dfbb94edf00a41a654
NagSun <nagsun@users.noreply.github.com>
2015-01-11 19:36:54 +0530

    initial commit

---
+++ server/example.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var db = require("mongojs").connect('SPAPP', ['groups', 'subgroups']);
+var app = express();
+
+
+// display Welcome message on homepage
+app.get('/', function(req, res){
+	res.send('Welcome to SPAPP');
+});
+
+app.get('/groups', function(req, res){
+	
+	db.groups.find({}, function(err, docs) {
+		var result={}; 
+		var docsLength = docs.length;
+		for(var i = 0; i < docsLength; i++ ) {
+			var id = docs[i]._id.valueOf();
+			id=id.toString();
+			result[id] = docs[i].name;			
+		}		
+		res.send(JSON.stringify(result));		
+	});
+});
+
+app.get('/subgroups', function(req, res) {
+	
+	db.subgroups.find({}, function(err, docs) {
+		var result={}; 
+		var docsLength = docs.length;
+		for(var i = 0; i < docsLength; i++ ) {
+			var id = docs[i].groupId;
+			id = id.toString();
+			var index = 0;
+			if (result[id]) {
+				index = result[id].length
+				result[id][index] = docs[i].name; 	
+			} else {
+				result[id] = [docs[i].name]; 
+			}
+						
+		}		
+		res.send(JSON.stringify(result));		
+	});
+});
+
+app.get('/add/group/:groupName', function(req, res){	
+	
+	db.groups.save({'name' : req.params.groupName}, 
+		function(err, saved) {
+		  if( err || !saved ) console.log("Error");
+		  else console.log("A new groups is added, thank you!");
+		});
+
+	res.send('Added new group');
+});
+
+app.get('/add/group/:groupId/subgroup/:subgroupName', function(req, res) {
+	// add subgroup to parent
+	db.subgroups.save({'groupId' : req.params.groupId, 'name' : req.params.subgroupName},
+		function(err, saved) {
+		  if( err || !saved ) console.log("Error");
+		  else console.log('Added subgroup %s to %s', req.params.subgroupName, req.params.groupId);
+		});
+
+	res.send('Added new subgroup');
+});
+
+// server listens on port 8000
+var server = app.listen(8000, function() {
+	console.log('Server listening at http://%s:%s', 
+		server.address().address,
+		server.address().port);	
+});
