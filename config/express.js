42563d58146f498c1274825970a9c7265de6a078
nagrajs <nagraj.sunkeri@gmail.com>
2015-01-23 00:28:28 +0530

    added node specific files

---
+++ config/express.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+ 	bodyParser = require('body-parser');
+
+module.exports = function() {
+  var app = express();
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
+
+
+  require('../app/routes/index.server.routes.js')(app);
+  return app;
+};
